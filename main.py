21d868ab4caba1cd07965ef2c9a488db49ef959e
MateoNitro550 <mandinoparedes@hotmail.com>
2024-01-14 11:53:57 -0500

    empower-u

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
 import os
-import jinja2
 import secrets
 import mysql.connector
 from passlib.hash import sha256_crypt
-from flask import Flask, jsonify, redirect, render_template, request, Response, session, url_for
+from flask import Flask, jsonify, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
-
-app.jinja_env.globals.update(zip=zip)
 
 secret_key = secrets.token_hex(16)
 app.secret_key = secret_key
@@ -21,32 +18,24 @@ db_config = {
     'database': os.environ.get('DB_NAME', 'database')
 }
 
-column_mapping = {
-    'application_name': 'APPLICATION NAME',
-    'assigned_to': 'ASSIGNED TO',
-    'category': 'CATEGORY',
-    'comments': 'COMMENTS',
-    'company': 'COMPANY',
-    'department': 'DEPARTMENT',
-    'expiration_date': 'EXPIRATION DATE',
-    'ip_address': 'IP ADDRESS',
-    'key_value': 'KEY VALUE',
-    'mobile_number': 'MOBILE NUMBER',
-    'model': 'MODEL',
-    'name': 'DEVICE NAME',
-    'purchase_date': 'PURCHASE DATE',
-    'snid': 'SNID',
-    'status': 'STATUS',
-    'tag': 'TAG',
-    'user': 'USER',
-    'audio_guides': 'Audio Guides',
-    'cell_phones': 'Cell Phones',
-    'certificates': 'Certificates',
-    'computers': 'Computers',
-    'monitors': 'Monitors',
-    'printers': 'Printers',
-    'stock': 'Stock'
-}
+questions = [
+    {'id': 1, 'category': 'Weight', 'text': '¿Cómo te sientes con tu peso actual?', 'options': ['Muy satisfecho/a', 'Satisfecho/a', 'Neutral', 'Insatisfecho/a', 'Muy insatisfecho/a'], 'value_mapping': {'Muy satisfecho/a': 0, 'Satisfecho/a': 1, 'Neutral': 2, 'Insatisfecho/a': 3, 'Muy insatisfecho/a': 4}},
+    {'id': 2, 'category': 'Weight', 'text': '¿Con qué frecuencia te preocupas por tu apariencia física?', 'options': ['Nunca', 'Ocasionalmente', 'A menudo', 'Frecuentemente', 'Siempre'], 'value_mapping': {'Nunca': 0, 'Ocasionalmente': 1, 'A menudo': 2, 'Frecuentemente': 3, 'Siempre': 4}},
+    {'id': 3, 'category': 'Weight', 'text': '¿Cómo te sientes acerca de tu apariencia física en comparación con tus pares?', 'options': ['Más que satisfecho/a', 'Satisfecho/a', 'Neutral', 'Insatisfecho/a', 'Muy insatisfecho/a'], 'value_mapping': {'Más que satisfecho/a': 0, 'Satisfecho/a': 1, 'Neutral': 2, 'Insatisfecho/a': 3, 'Muy insatisfecho/a': 4}},
+    {'id': 4, 'category': 'Weight', 'text': '¿Has experimentado comentarios negativos sobre tu cuerpo?', 'options': ['Nunca', 'Ocasionalmente', 'A veces', 'Frecuentemente', 'Siempre'], 'value_mapping': {'Nunca': 0, 'Ocasionalmente': 1, 'A veces': 2, 'Frecuentemente': 3, 'Siempre': 4}},
+    {'id': 5, 'category': 'Academic Pressure', 'text': '¿Cómo describirías el nivel de presión que sientes en tus estudios?', 'options': ['Muy bajo', 'Bajo', 'Moderado', 'Alto', 'Muy alto'], 'value_mapping': {'Muy bajo': 0, 'Bajo': 1, 'Moderado': 2, 'Alto': 3, 'Muy alto': 4}},
+    {'id': 6, 'category': 'Academic Pressure', 'text': '¿Cuánto te afecta el estrés académico en tu vida diaria?', 'options': ['No afecta', 'Levemente afecta', 'Moderadamente afecta', 'Bastante afecta', 'Muy afecta'], 'value_mapping': {'No afecta': 0, 'Levemente afecta': 1, 'Moderadamente afecta': 2, 'Bastante afecta': 3, 'Muy afecta': 4}},
+    {'id': 7, 'category': 'Academic Pressure', 'text': '¿Sientes presión para tomar decisiones importantes sobre tu futuro?', 'options': ['No', 'Un poco', 'Moderadamente', 'Bastante', 'Mucho'], 'value_mapping': {'No': 0, 'Un poco': 1, 'Moderadamente': 2, 'Bastante': 3, 'Mucho': 4}},
+    {'id': 8, 'category': 'Academic Pressure', 'text': '¿Te sientes presionado/a por las expectativas de los demás en cuanto a tu rendimiento personal?', 'options': ['No', 'Poco', 'Moderadamente', 'Bastante', 'Mucho'], 'value_mapping': {'No': 0, 'Poco': 1, 'Moderadamente': 2, 'Bastante': 3, 'Mucho': 4}},
+    {'id': 9, 'category': 'Emotional Health', 'text': '¿Cómo describirías tu estado de ánimo general?', 'options': ['Muy positivo', 'Positivo', 'Neutral', 'Negativo', 'Muy negativo'], 'value_mapping': {'Muy positivo': 0, 'Positivo': 1, 'Neutral': 2, 'Negativo': 3, 'Muy negativo': 4}},
+    {'id': 10, 'category': 'Emotional Health', 'text': '¿Con qué frecuencia experimentas síntomas de ansiedad o tristeza?', 'options': ['Nunca', 'Ocasionalmente', 'A veces', 'Frecuentemente', 'Siempre'], 'value_mapping': {'Nunca': 0, 'Ocasionalmente': 1, 'A veces': 2, 'Frecuentemente': 3, 'Siempre': 4}},
+    {'id': 11, 'category': 'Emotional Health', 'text': '¿Te sientes cómodo/a expresando tus emociones con amigos o familiares?', 'options': ['Muy cómodo/a', 'Cómodo/a', 'Neutral', 'Incómodo/a', 'Muy incómodo/a'], 'value_mapping': {'Muy cómodo/a': 0, 'Cómodo/a': 1, 'Neutral': 2, 'Incómodo/a': 3, 'Muy incómodo/a': 4}},
+    {'id': 12, 'category': 'Emotional Health', 'text': '¿Cómo manejas el estrés en tu vida diaria?', 'options': ['Estrategias efectivas', 'Estrategias moderadamente efectivas', 'Estrategias poco efectivas', 'Sin estrategias definidas', 'No manejo el estrés'], 'value_mapping': {'Estrategias efectivas': 0, 'Estrategias moderadamente efectivas': 1, 'Estrategias poco efectivas': 2, 'Sin estrategias definidas': 3, 'No manejo el estrés': 4}},
+    {'id': 13, 'category': 'Interpersonal Relationships', 'text': '¿Sientes presión social para encajar con ciertos grupos?', 'options': ['Nada de presión', 'Poca presión', 'Moderada', 'Alta presión', 'Muy alta presión'], 'value_mapping': {'Nada de presión': 0, 'Poca presión': 1, 'Moderada': 2, 'Alta presión': 3, 'Muy alta presión': 4}},
+    {'id': 14, 'category': 'Interpersonal Relationships', 'text': '¿Tienes personas en las que confías para hablar sobre tus inseguridades?', 'options': ['Sí, varias personas', 'Sí, una o dos personas', 'No estoy seguro/a', 'No, pero me gustaría tener alguien', 'No, prefiero no compartir'], 'value_mapping': {'Sí, varias personas': 0, 'Sí, una o dos personas': 1, 'No estoy seguro/a': 2, 'No, pero me gustaría tener alguien': 3, 'No, prefiero no compartir': 4}},
+    {'id': 15, 'category': 'Interpersonal Relationships', 'text': '¿Tienes dificultades para hacer amigos o sentirte aceptado/a en grupos sociales?', 'options': ['No', 'Ocasionalmente', 'A veces', 'Frecuentemente', 'Siempre'], 'value_mapping': {'No': 0, 'Ocasionalmente': 1, 'A veces': 2, 'Frecuentemente': 3, 'Siempre': 4}},
+    {'id': 16, 'category': 'Interpersonal Relationships', 'text': '¿Cómo te afecta la soledad en tu bienestar emocional?', 'options': ['No me afecta', 'Levemente afecta', 'Moderadamente afecta', 'Bastante afecta', 'Muy afecta'], 'value_mapping': {'No me afecta': 0, 'Levemente afecta': 1, 'Moderadamente afecta': 2, 'Bastante afecta': 3, 'Muy afecta': 4}},
+]
 
 def execute_query(query, params=None, operation="select"):
     # Connect to the database
@@ -75,49 +64,8 @@ def execute_query(query, params=None, operation="select"):
         conn.close()
         return str(e)
 
-def get_all_categories():
-    query = "SHOW TABLES"
-    result = execute_query(query)
-
-    categories = [row[0] for row in result if row[0] != 'users']
-
-    return categories
-
-def get_category_data(category):
-    # Fetch columns for the selected category
-    columns_query = f"SHOW COLUMNS FROM {category}"
-    columns_result = execute_query(columns_query)
-    columns = [row[0] for row in columns_result]
-
-    # Fetch data for the selected category
-    data_query = f"SELECT * FROM {category}"
-    devices = execute_query(data_query)
-
-    return columns, devices
-
-@app.route('/get_columns/<category>')
-def get_columns_endpoint(category):
-    columns, _ = get_category_data(category)
-    return jsonify({'columns': columns})
-    return columns, devices
-
-@app.route('/get_total_count_data')
-def get_total_count_data():
-    tables_query = "SHOW TABLES"
-    tables_result = execute_query(tables_query)
-    tables = [table[0] for table in tables_result if table[0] != 'users']
-
-    table_counts = []
-
-    for table in tables:
-        count_query = f"SELECT COUNT(*) FROM {table}"
-        count_result = execute_query(count_query)
-        count = count_result[0][0]
-        table_counts.append((column_mapping.get(table, table), table, count))
-
-    labels_mapped, labels_original, counts = zip(*table_counts)
-
-    return jsonify({'labels_mapped': labels_mapped, 'labels_original': labels_original, 'values': counts})
+def hash_password(password):
+    return sha256_crypt.hash(password)
 
 @app.route('/')
 def index():
@@ -129,21 +77,119 @@ def login():
         input_username = request.form['username']
         input_password = request.form['password']
 
-        check_user_query = "SELECT username, password, role FROM users WHERE username = %s"
+        check_user_query = "SELECT id, username, password, questionnaire FROM users WHERE username = %s"
         user_data = execute_query(check_user_query, (input_username,))
 
-        stored_username = None
-
         if user_data:
-            stored_username, stored_password, role = user_data[0]
+            user_id, stored_username, stored_password, questionnaire = user_data[0]
 
-        if input_username == stored_username and sha256_crypt.verify(input_password, stored_password):
-            # Store the user's role in the session
-            session['role'] = role
-            session['logged_in'] = True
-            return redirect(url_for('dashboard'))
+            if sha256_crypt.verify(input_password, stored_password):
+                if not questionnaire:
+                    session['logged_in'] = True
+                    session['username'] = input_username
+                    return redirect(url_for('questionnaire'))
+
+                session['logged_in'] = True
+                return redirect(url_for('dashboard'))
 
     return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error_message = None
+
+    if request.method == 'POST':
+        input_username = request.form['username']
+        input_password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        if input_password != confirm_password:
+            error_message = "Las contraseñas no coinciden"
+
+        else:
+            check_username_query = "SELECT id FROM users WHERE username = %s"
+            existing_user = execute_query(check_username_query, (input_username,))
+
+            if existing_user:
+                error_message = "El nombre de usuario ya existe. Por favor, elige otro."
+
+            else:
+                hashed_password = hash_password(input_password)
+
+                insert_user_query = "INSERT INTO users (username, password, questionnaire) VALUES (%s, %s, %s)"
+                execute_query(insert_user_query, (input_username, hashed_password, 0), operation="insert")
+
+                return redirect(url_for('login'))
+
+    return render_template('register.html', error=error_message)
+
+@app.route('/questionnaire', methods=['GET', 'POST'])
+def questionnaire():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        results = {}
+        for question in questions:
+            answer = request.form.get(f'answer_{question["id"]}')
+            if answer:
+                category = question['category']
+                value = question['value_mapping'][answer]
+                results[category] = results.get(category, 0) + value
+
+        highest_insecurity_categories = []
+        max_score = max(results.values())
+
+        for category, score in results.items():
+            if score == max_score:
+                highest_insecurity_categories.append(category)
+
+        if max_score == 0:
+            highest_insecurity_categories = []
+
+        """
+        return jsonify({
+            'final_results': results,
+            'highest_insecurity_categories': highest_insecurity_categories
+        })
+        """
+
+        input_username = session.get('username')
+        update_questionnaire_query = "UPDATE users SET questionnaire = 1 WHERE username = %s"
+        update_insecurity_query = "UPDATE users SET insecurity = %s WHERE username = %s"
+        execute_query(update_questionnaire_query, (input_username,), operation="update")
+
+        highest_insecurity_categories_str = ', '.join(highest_insecurity_categories)
+
+        if len(highest_insecurity_categories) == 1:
+            highest_insecurity_categories_str = highest_insecurity_categories[0]
+
+        execute_query(update_insecurity_query, (highest_insecurity_categories_str, input_username), operation="update")
+
+        return redirect(url_for('dashboard'))
+
+
+    return render_template('questionnaire.html', questions=questions)
+
+@app.route('/tips')
+def tips():
+    return render_template('tips.html')
+
+@app.route('/ai')
+def ai():
+    return render_template('ai.html')
+
+@app.route('/experts')
+def experts():
+    return render_template('experts.html')
+
+@app.route('/community')
+def community():
+    return render_template('community.html')
 
 @app.route('/logout')
 def logout():
@@ -151,153 +197,6 @@ def logout():
     if 'timeout' in request.args:
         return render_template('timeout.html')
     return redirect(url_for('index'))
-
-@app.route('/dashboard')
-def dashboard():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-
-    categories = get_all_categories()
-
-    total_count_query = "SELECT SUM(cnt) FROM ("
-    for category in categories:
-        total_count_query += f"SELECT COUNT(*) as cnt FROM {category} UNION ALL "
-    total_count_query = total_count_query[:-10] + ") as total"
-    total_count_result = execute_query(total_count_query)
-    total_count = total_count_result[0][0] if total_count_result else 0
-
-    if session['role'] == 'admin':
-        return render_template('dashboard.html', categories=categories, column_mapping=column_mapping, total_count=total_count, is_admin=True)
-    else:
-        return render_template('dashboard.html', categories=categories, column_mapping=column_mapping, total_count=total_count, is_admin=False)
-
-@app.route('/manage-users')
-def manageUsers():
-    return render_template('manageUsers.html')
-
-@app.route('/segmentation/<category>')
-def segmentation(category):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-
-    columns, devices = get_category_data(category)
-
-    if session['role'] == 'admin':
-        return render_template('segmentation.html', category=category, column_mapping=column_mapping, columns=columns, devices=devices, is_admin=True, table_name=column_mapping.get(category, category))
-    else:
-        return render_template('segmentation.html', category=category, column_mapping=column_mapping, columns=columns, devices=devices, is_admin=False, table_name=column_mapping.get(category, category))
-
-@app.route('/add_device', methods=['POST'])
-def add_device():
-    if not session.get('logged_in') or session['role'] != 'admin':
-        return redirect(url_for('login'))
-
-    if request.method == 'POST':
-        currentCategory = request.form.get('category')
-
-        columns, _ = get_category_data(currentCategory)
-
-        valid_columns = columns[1:]
-
-        values = [request.form.get(column) for column in valid_columns]
-
-        insert_query = f"INSERT INTO {currentCategory} ({', '.join(valid_columns)}) VALUES ({', '.join(['%s']*len(valid_columns))})"
-
-        insert = execute_query(insert_query, values, operation="insert")
-
-        if insert:
-            return redirect(url_for('segmentation', category=currentCategory))
-        else:
-            return "Failed to add the device. Please try again."
-
-    return render_template('segmentation.html')
-
-@app.route('/update_device', methods=['POST'])
-def update_device():
-    try:
-        data = request.get_json()
-        
-        device_id = data.get('id')
-        category = data.get('category')
-
-        updated_data = {key: data.get(key) for key in data.keys() - {'id', 'category'}}
-
-        set_clause = ', '.join([f"{key} = %s" for key in updated_data.keys()])
-
-        update_query = f"UPDATE {category} SET {set_clause} WHERE id = %s"
-
-        update_data = list(updated_data.values()) + [device_id]
-
-        update = execute_query(update_query, update_data, "update")
-
-        if update:
-            return jsonify({'success': True, 'message': 'Device updated successfully'})
-        else:
-            return jsonify({'success': False, 'error': 'Failed to update device'})
-
-    except Exception as e:
-        print('Exception:', str(e))
-        return jsonify({'success': False, 'error': 'Invalid data format'})
-
-@app.route('/delete_device', methods=['POST'])
-def delete_device():
-    try:
-        data = request.get_json()
-        device_id = data.get('id')
-        category = data.get('category')
-
-        delete_query = f"DELETE FROM {category} WHERE id = %s"
-        delete_data = (device_id,)
-
-        delete = execute_query(delete_query, delete_data, "delete")
-
-        if delete:
-            return jsonify({'success': True, 'message': 'Device deleted successfully'})
-        else:
-            return jsonify({'success': False, 'error': 'Failed to delete device'})
-
-    except Exception as e:
-        print('Exception:', str(e))
-        return jsonify({'success': False, 'error': 'Invalid data format'})
-
-@app.route('/move_to_stock', methods=['POST'])
-def move_to_stock():
-    try:
-        data = request.get_json()
-
-        device_info = data.get('deviceInfo', {})
-
-        snid = device_info.get('SNID')
-        model = device_info.get('Model')
-
-        insert_query = "INSERT INTO stock (SNID, MODEL) VALUES (%s, %s)"
-        insert_data = (snid, model)
-
-        success = execute_query(insert_query, insert_data, operation="insert")
-
-        if success:
-            return jsonify({'success': True, 'message': 'Device moved to stock successfully'})
-        else:
-            return jsonify({'success': False, 'error': 'Failed to move device to stock'})
-
-    except Exception as e:
-        print('Exception:', str(e))
-        return jsonify({'success': False, 'error': 'Failed to move device to stock'})
-
-@app.route('/report/<category>')
-def download_csv(category):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-
-    columns, devices = get_category_data(category)
-
-    csv_data = ','.join([column_mapping.get(column, column) for column in columns[1:]]) + '\n'
-    csv_data += '\n'.join([','.join(map(str, device[1:])) for device in devices])
-
-    response = Response(csv_data, mimetype='text/csv')
-    response.headers['Content-Disposition'] = f'attachment; filename={category}_report.csv'
-
-    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
